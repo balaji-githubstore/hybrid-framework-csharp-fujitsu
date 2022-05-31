2bb8170cfa08b353a7cd03c45e890e807523cc51
balaji0017 <balaji0017@gmail.com>
2022-05-31 17:32:02 +0530

    testcasesource for invalid  credential test

--- OrangeAutomation/LoginTest.cs
+++ OrangeAutomation/LoginTest.cs
@@ -36,9 +36,24 @@ namespace Fujitsu.OrangeAutomation
             //("John","John123","Invalid credentials")
             //("Peter", "Peter123", "Invalid credentials")
 
+            object[] data1 = new object[3];
+            data1[0] = "John";
+            data1[1] = "John123";
+            data1[2] = "Invalid credentials";
+
+            object[] data2 = new object[3];
+            data2[0] = "Peter";
+            data2[1] = "Pete123";
+            data2[2] = "Invalid credentials";
+
+            object[] main = new object[2];
+            main[0] = data1;
+            main[1] = data2;
+
+            return main;
         }
 
-        [Test]
+        [Test,TestCaseSource(nameof(InvalidCredentialData))]
         //[TestCase("John","John123","Invalid credentials")]
         //[TestCase("Peter", "Peter123", "Invalid credentials")]
         public void InvalidCredentialTest(string username,string password,string expectedError)
