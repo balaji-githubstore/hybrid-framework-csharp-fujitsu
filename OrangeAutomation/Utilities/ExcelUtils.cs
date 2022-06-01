81da5363bc8ed3d8bc378e535b96e23498dd1863
balaji0017 <balaji0017@gmail.com>
2022-06-01 16:47:36 +0530

    sheet to object[] method created

---
+++ OrangeAutomation/Utilities/ExcelUtils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace OrangeAutomation.Utilities
+{
+    public class ExcelUtils
+    {
+        public static object[] GetSheetIntoObjectArray()
+        {
+            using (XLWorkbook book = new XLWorkbook(@"C:\Selenium Session\OrangeAutomation\OrangeAutomation\TestData\orange_data.xlsx"))
+            {
+                var sheet = book.Worksheet("InvalidCredentialTest");
+                var range = sheet.RangeUsed();
+
+                int rowCount = range.RowCount();
+                int columnCount = range.ColumnCount();
+                //size is based on numbe of test case - (rowcount-1)
+                object[] main = new object[rowCount - 1];
+
+                for (int r = 2; r <= rowCount; r++)
+                {
+                    //size is based on colcount //number of arguments
+                    object[] set = new object[columnCount];
+
+                    for (int c = 1; c <= columnCount; c++)
+                    {
+                        string value = range.Cell(r, c).GetString();
+ 
+                        set[c - 1] = value;
+                    }
+                    main[r - 2] = set;
+                }
+                return main;
+            }
+            
+        }
+    }
+}
