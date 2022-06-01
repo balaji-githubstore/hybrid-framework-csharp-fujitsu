c5bc1e509795dcc84bab4eb6d088a415c35a2791
balaji0017 <balaji0017@gmail.com>
2022-06-01 17:11:16 +0530

    invalid credential test connected with excel

--- OrangeAutomation/Utilities/DataUtils.cs
+++ OrangeAutomation/Utilities/DataUtils.cs
@@ -14,26 +14,9 @@ namespace Fujitsu.OrangeAutomation.Utilities
         /// <returns>return object[] </returns>
         public static object[] InvalidCredentialData()
         {
-            object[] main = new object[2];
-
-
-            object[] set1 = new object[3];
-            set1[0] = "John";
-            set1[1] = "John123";
-            set1[2] = "Invalid credentials";
-
-            main[0] = set1;
-
-            object[] set2 = new object[3];
-            set2[0] = "Peter";
-            set2[1] = "Pete123";
-            set2[2] = "Invalid credentials";
-
-            main[1] = set2;
-
+            object[] main = ExcelUtils.GetSheetIntoObjectArray(@"C:\Selenium Session\OrangeAutomation\OrangeAutomation\TestData\orange_data.xlsx", "InvalidCredentialTest");
             return main;
         }
-
         public static object[] EmergencyContactData()
         {
             object[] set1 = new object[8];
@@ -62,6 +45,35 @@ namespace Fujitsu.OrangeAutomation.Utilities
 
             return main;
         }
+
+        //public static object[] EmergencyContactData()
+        //{
+        //    object[] set1 = new object[8];
+        //    set1[0] = "Admin";
+        //    set1[1] = "admin123";
+        //    set1[2] = "Sat";
+        //    set1[3] = "Brother";
+        //    set1[4] = "3434";
+        //    set1[5] = "3434";
+        //    set1[6] = "3434";
+        //    set1[7] = "Sat;Brother;3434";
+
+        //    object[] set2 = new object[8];
+        //    set2[0] = "Admin";
+        //    set2[1] = "admin123";
+        //    set2[2] = "John";
+        //    set2[3] = "Brother";
+        //    set2[4] = "53434";
+        //    set2[5] = "53434";
+        //    set2[6] = "53434";
+        //    set2[7] = "John;Brother;53434";
+
+        //    object[] main = new object[2];
+        //    main[0] = set1;
+        //    main[1] = set2;
+
+        //    return main;
+        //}
 
 
     }
